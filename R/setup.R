b7ecf5ee02ba0ff1542de0ffc24359c6c4d21fe1
Danielle Navarro <d.navarro@unsw.edu.au>
2019-06-21 14:56:40 +1000

    supply general purpose "add_trial" function

--- R/setup.R
+++ R/setup.R
@@ -1,30 +1,30 @@
 #' Initialise an experiment
 #' @export
-xpt_create <- function() {
+jspsych_create <- function() {
   xpt <- list()
   xpt$head <- list()
-  xpt$head$script <- system.file("extdata", "jspsych.js", package = "xprmntr", mustWork = TRUE)
-  xpt$head$style <- system.file("extdata", "css", "jspsych.css", package = "xprmntr", mustWork = TRUE)
+  xpt$head$script <- "jspsych.js"
+  xpt$head$style <- file.path("css", "jspsych.css")
   xpt$trial <- list()
+  xpt$init <- list()
   return(xpt)
 }
-
 #' Add trial that shows an image and respond with keyboard
 #' @param xpt the experiment object
 #' @param stimulus path to an image file
 #' @param choices numeric codes for allowed responses
 #' @param prompt text of the response prompt
 #' @export
-xpt_image_keyboard_response <-function(xpt, stimulus, choices, prompt) {
+jspsych_add <-function(xpt, type, options = list()) {
 
-  xpt$trial[[length(xpt$trial) + 1]] <- list(
-    type = "image-keyboard-response",
-    stimulus = stimulus,
-    choices = choices,
-    prompt = prompt
-  )
+  # append the trial information to the list
+  trial_number <- length(xpt$trial) + 1
+  xpt$trial[[trial_number]] <- c(list(type = type), options)
 
-  plugin <- system.file("extdata", "plugins", "jspsych-image-keyboard-response.js", package = "xprmntr", mustWork = TRUE)
+  # specify path to plugin file
+  plugin <- file.path("plugins", paste0("jspsych-", type, ".js"))
+
+  # make sure the plugin file is listed
   if(!plugin %in% xpt$head$script) {
     xpt$head$script <- c(xpt$head$script, plugin)
   }
@@ -32,76 +32,76 @@ xpt_image_keyboard_response <-function(xpt, stimulus, choices, prompt) {
   return(xpt)
 }
 
+#' Construct a call to jsPsych.init
+#'
+#'
+jspsych_init <- function(xpt) {
+
+
+}
+
+
 
 #' Convert the experiment object to HTML file
 #'
 #' @param  xpt the experiment object
 #' @export
-xpt_write <- function(xpt, file) {
+jspsych_write <- function(xpt, path) {
 
-  # initialise
+  # create experiment folder with jspsych files
+  dir.create(path)
+  utils::unzip(
+    zipfile = system.file("extdata", "jspsych-6.0.5.zip", package = "xprmntr"),
+    exdir = path)
+
+  # write the trial list to a js file
+  tl <- jspsychify_timeline(xpt$trial)
+
+  tl <- c(tl, "jsPsych.init({
+    timeline: [trial_1, trial_2, trial_3],
+    default_iti: 250
+  });")
+
+  writeLines(tl, file.path(path, "experiment.js"))
+
+  # header info for html file
   html <- c(
     '<!DOCTYPE html>',
-    '  <html>'
-  )
-
-  # add scripts style files in the head
-  html <- c(
-    html,
+    '  <html lang="en-us">',
     '  <head>',
-    paste0('    <script src="', xpt$head$script, '"></script>'),
+    paste0('    <script src = "', xpt$head$script, '"></script>'),
+    paste0('    <script src = "experiment.js"></script>'),
     paste0('    <link rel="stylesheet" href="', xpt$head$style, '">'),
-    '  </head>'
-  )
-
-  # the body is empty
-  html <- c(
-    html,
+    '  </head>',
     '  <body>',
-    '  </body>'
-  )
-
-  # open script tag defining trials
-  html <- c(
-    html,
-    '  <script>'
-  )
-
-  # add trials
-  ntr <- length(xpt$trial)
-  for(tr in 1:ntr) {
-    html <- c(
-      html,
-      '',
-      paste0('var trial_', tr, ' = {'),
-      '  type: "image-keyboard-response",',
-      paste0('  stimulus: "', xpt$trial[[tr]]$stimulus , '",'),
-      paste0('  choices: [', paste(xpt$trial[[tr]]$choices, collapse = ",") ,'],'),
-      paste0('  prompt: "', xpt$trial[[tr]]$prompt , '",'),
-      '}'
-    )
-  }
-
-  # create timeline object
-  html <- c(
-    html,
-    '',
-    'jsPsych.init({',
-    paste0('  timeline: [', paste0("trial_", 1:ntr, collapse = ", "),'],'),
-    '  default_iti: 250',
-    '});'
-  )
-
-  # close script tag defining trials & document
-  html <- c(
-    html,
-    '  </script>',
+    '  </body>',
     '</html>'
   )
 
   # write the file
-  writeLines(html, file)
+  writeLines(html, file.path(path, "index.html"))
 
   # invisibly return the input
   return(invisible(xpt))
 }
+
+
+# create the timeline string
+jspsychify_timeline <- function(timeline) {
+
+  ntr <- length(timeline)
+  tl_str <- character()
+  for(tr in 1:ntr) {
+    tl_str <- c(tl_str, '', paste0(
+        'var trial_', tr, ' = ',
+        jspsychify_trial(xpt$trial[[tr]]) , ";")
+    )
+  }
+  return(tl_str)
+}
+
+# convert a single trial to an appropriate json string
+jspsychify_trial <- function(trial) {
+  jsonlite::toJSON(trial, auto_unbox = TRUE)
+}
+
--- misc/demo1.R
+++ misc/demo1.R
@@ -2,16 +2,21 @@ library(xprmntr)
 library(magrittr)
 
 # stimuli, prompt, response options
-img <- file.path(system.file("extdata", package = "rainbowr"), c("rainbow.svg", "transgender.svg", "bisexual.svg"))
-txt <- "Do you like this flag? (Y or N)"
-opt <- c(89, 78)
+my_opts <- function(ind) {
+  images <- c("rainbow.svg", "transgender.svg", "bisexual.svg")
+  return(list(
+    stimulus = file.path(system.file("extdata", package = "rainbowr"), images[ind]),
+    prompt = "Do you like this flag? (Y or N)",
+    choices = c(89, 78)
+  ))
+}
 
 # create an experiment with three trials
-xpt <- xpt_create() %>%
-  xpt_image_keyboard_response(stimulus = img[1], choices = opt, prompt = txt) %>%
-  xpt_image_keyboard_response(stimulus = img[2], choices = opt, prompt = txt) %>%
-  xpt_image_keyboard_response(stimulus = img[3], choices = opt, prompt = txt)
+xpt <- jspsych_create() %>%
+  jspsych_add(type = "image-keyboard-response", options = my_opts(1)) %>%
+  jspsych_add(type = "image-keyboard-response", options = my_opts(2)) %>%
+  jspsych_add(type = "image-keyboard-response", options = my_opts(3))
 
 # write it to an html file
-xpt_write(xpt, "~/Desktop/expt.html")
+jspsych_write(xpt, "~/Desktop/expelllr")
 
