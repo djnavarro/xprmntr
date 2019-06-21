588234b1c4330309ff3042e8d790193932241779
Danielle Navarro <d.navarro@unsw.edu.au>
2019-06-21 20:47:22 +1000

    tidy demo 1 and readme

--- misc/demo1.R
+++ misc/demo1.R
@@ -1,16 +1,22 @@
 library(xprmntr)
 library(magrittr)
 
+
+# vector of file paths to the resources to be included
 img <- c("rainbow.svg", "transgender.svg", "bisexual.svg")
 img <- file.path(system.file("extdata", package = "rainbowr"), img)
-prp <- "Do you like this flag? (Y or N)"
-chc <- c(89, 78)
 
 # create an experiment with three trials
 xpt <- jspsych_create(files = img) %>%
-  jspsych_add(type = "image-keyboard-response", stimulus = res("rainbow.svg"), prompt = prp, choices = chc) %>%
-  jspsych_add(type = "image-keyboard-response", stimulus = res("transgender.svg"), prompt = prp, choices = chc) %>%
-  jspsych_add(type = "image-keyboard-response", stimulus = res("bisexual.svg"), prompt = prp, choices = chc) %>%
+  jspsych_add(type = "image-keyboard-response", stimulus = res("rainbow.svg"),
+              prompt = "Do you like this flag? (Y or N)", choices = c(89, 78)) %>%
+  jspsych_add(type = "image-keyboard-response", stimulus = res("transgender.svg"),
+              prompt = "Do you like this flag? (Y or N)", choices = c(89, 78)) %>%
+  jspsych_add(type = "image-keyboard-response", stimulus = res("bisexual.svg"),
+              prompt = "Do you like this flag? (Y or N)", choices = c(89, 78)) %>%
+  jspsych_add(type = "survey-likert", questions = list(list(
+    prompt = "How much did you enjoy this?",
+    labels = c("a lot", "not much", "not at all"), required = TRUE))) %>%
   jspsych_init(default_iti = 250)
 
 # write it to an html file
