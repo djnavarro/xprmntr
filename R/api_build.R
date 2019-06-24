995894f0609e7f69b35d701e41e5a8440bdb39c5
Danielle Navarro <d.navarro@unsw.edu.au>
2019-06-24 15:01:23 +1000

    localrun api writes data to file

--- R/api_build.R
+++ R/api_build.R
@@ -11,11 +11,19 @@ run <- function(path = ".") {
   pr <- plumber::plumber$new()
 
   static_site <- file.path(path, "experiment")
+  data_folder <- file.path(path, "data")
   static_router <- plumber::PlumberStatic$new(static_site)
 
   pr$mount("/", static_router)
   pr$handle("POST", "/submit", function(req, res){
-    print(req$postBody)
+
+    dat <- jsonlite::fromJSON(req$postBody)
+    dat <- readr::read_csv(dat$filedata)
+    tsp <- get_timestamp()
+    file_id <- paste("data", get_timestamp(), get_alphanumeric(10), sep = "_")
+    dat$file_id <- file_id
+    dat <- dat[, c(ncol(dat), 1:ncol(dat)-1), drop = FALSE]
+    readr::write_csv(dat, file.path(data_folder, paste0(file_id, ".csv")))
   })
 
   pr$run(swagger = FALSE)
--- R/helpers.R
+++ R/helpers.R
@@ -19,3 +19,18 @@ unquote <- function(x) {
   class(x) <- "json"
   x
 }
+
+
+get_timestamp <- function() {
+  tsp <- as.character(Sys.time())
+  tsp <- gsub("[ :]", "-", tsp)
+  return(tsp)
+}
+
+get_alphanumeric <- function(n = 5) {
+  x <- c("a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r",
+         "s","t","u","v","w","x","y","z","0","1","2","3","4","5","6","7","8","9")
+  r <- sample(x, size = n, replace = TRUE)
+  r <- paste(r, collapse = "")
+  return(r)
+}
