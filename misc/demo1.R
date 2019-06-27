97cdadbae675ddacf5ce577cf47ff4656e171906
Danielle Navarro <d.navarro@unsw.edu.au>
2019-06-27 11:49:52 +1000

    demo uses new xprmtr.save_locally

--- misc/demo1.R
+++ misc/demo1.R
@@ -33,7 +33,7 @@ xpt <- jspsych_create(files = img) %>%
     ) %>%
   jspsych_init(
     default_iti = 250,
-    on_finish = unquote("xprmntr.localsave"))
+    on_finish = unquote("xprmntr.save_locally"))
 
 # write it to an html file
 jspsych_write(xpt, "~/Desktop/expt")
