2f8d0edc84798e3f281fe6d2fed265b9d8035766
Danielle Navarro <d.navarro@unsw.edu.au>
2019-09-24 15:51:13 +1000

    update demo

--- misc/demo1.R
+++ misc/demo1.R
@@ -1,44 +1,72 @@
 library(xprmntr)
 
-resources <- file.path(system.file("extdata", "img", package = "xprmntr"))
-images <- c("bisexual.svg", "transgender.svg", "rainbow.svg")
+# resource folder
+resources <- system.file("extdata", "img", package = "xprmntr")
 
+# variables to use
+flag_images <- c("bisexual.svg", "transgender.svg", "rainbow.svg")
+flag_names <- c("bisexual", "transgender", "LGBT")
+
+# define a welcome trial
+welcome <- trial(
+  type = "html-keyboard-response",
+  stimulus = "Welcome to the experiment! Press any key to continue",
+  choices = code("jsPsych.ANY_KEY")
+)
+
+# define a fixation trial
 fixation <- trial(
   type = "html-keyboard-response",
   stimulus = '<div style="font-size:60px;">+</div>',
-  choices =  unquote("jsPsych.NO_KEYS"),
+  choices =  code("jsPsych.NO_KEYS"),
   trial_duration = 500
 )
 
-test <- trial(
+# define a test trial
+query <- trial(
   type = "image-keyboard-response",
   stimulus = use_variable("stimulus"),
-  prompt = "Do you like this flag? ('y' or 'n')",
+  prompt = use_variable("prompt"),
   choices = c('y', 'n')
 )
 
-test_procedure <- timeline(fixation, test) %>%
-  with_variable(name = "stimulus", values = resource(images)) #%>%
-  #with_parameters(randomize_order = TRUE, repetitions = 2)
+# nested timelines with repetitions and variables
+testing <- timeline(fixation, query) %>%
+  with_variables(
+    prompt = paste("is this the", flag_names, "flag? (y/n)"),
+    stimulus = resource(flag_images)) %>%
+  with_parameters(randomize_order = TRUE, repetitions = 2)
 
+# post experimental questionnaire
+survey <- trial(
+  type = "survey-likert",
+  questions = list(list(
+    prompt = "How much did you enjoy this?",
+    labels = c("a lot", "not much", "not at all"),
+    required = TRUE
+  ))
+)
 
-# make experiment handles both jsPsych.init and write
+# define an end of experiment trial
+finish <- trial(
+  type = "html-keyboard-response",
+  stimulus = "All done!",
+  choices = code("jsPsych.ANY_KEY")
+)
+
+# everything
+experiment <- timeline(welcome, testing, survey, finish)
+
+# construct the experiment
 make_experiment(
-  timeline = test_procedure,
+  timeline = experiment,
   path = "~/Desktop/expt",
   resources = resources,
   default_iti = 250,
-  on_finish = unquote("xprmntr.save_locally")
+  on_finish = code("xprmntr.save_locally")
 )
 
-#run_locally("~/Desktop/expt")
-
-# %>%
-#   add_trial(
-#     type = "survey-likert",
-#     questions = list(list(
-#       prompt = "How much did you enjoy this?",
-#       labels = c("a lot", "not much", "not at all"),
-#       required = TRUE
-#     ))
-#   )
+run_locally(
+  path = "~/Desktop/expt",
+  port = 8000
+)
