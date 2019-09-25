77d03b3e823fbd56ca3ee4ec16b8894ab25d30c9
Danielle Navarro <d.navarro@unsw.edu.au>
2019-09-25 19:06:50 +1000

    adds image_slider_response and tidies

--- R/trial_html.R
+++ R/trial_html.R
@@ -1,4 +1,53 @@
 
+#' HTML stimulus, button response
+#'
+#' @param stimulus HTML stimulus
+#'
+#' @param choices Character vector with labels for the buttons
+#' @param button_html HTML templay specifying the buttons (defaults to jsPsych default)
+#' @param margin_vertical Vertical margin of the buttons (default "0px")
+#' @param margin_horizontal Horizontal margin of the buttons (default "8px")
+#'
+#' @param prompt The prompt to be displayed with the stimulus (can include HTML markup)
+#' @param stimulus_duration How long to display stimulus in milliseconds? (default is to remain until response)
+#' @param trial_duration How long to wait for response in milliseconds? (default is to wait indefinitely)
+#' @param response_ends_trial Does the trial end with response (default = TRUE) or continue until trial duration reached?
+#'
+#' @export
+html_button_response <- function(
+  stimulus,
+
+  choices,
+  button_html = NULL,
+  margin_vertical = "0px",
+  margin_horizontal = "8px",
+
+  prompt = NULL,
+  stimulus_duration = NULL,
+  trial_duration = NULL,
+  response_ends_trial = TRUE
+) {
+  drop_nulls(
+    trial(
+      type = "html-keyboard-response",
+
+      stimulus = stimulus,
+
+      choices = choices,
+      button_html = button_html,
+      margin_vertical = margin_vertical,
+      margin_horizontal = margin_horizontal,
+
+      prompt = prompt,
+      stimulus_duration = stimulus_duration,
+      trial_duration = trial_duration,
+      response_ends_trial = response_ends_trial
+    )
+  )
+}
+
+
+
 #' HTML stimulus, keyboard response
 #'
 #' @param stimulus HTML stimulus
@@ -13,7 +62,9 @@
 #' @export
 html_keyboard_response <- function(
   stimulus,
+
   choices = any_key(),
+
   prompt = NULL,
   stimulus_duration = NULL,
   trial_duration = NULL,
@@ -22,53 +73,11 @@ html_keyboard_response <- function(
   drop_nulls(
     trial(
       type = "html-keyboard-response",
+
       stimulus = stimulus,
+
       choices = choices,
-      prompt = prompt,
-      stimulus_duration = stimulus_duration,
-      trial_duration = trial_duration,
-      response_ends_trial = response_ends_trial
-    )
-  )
-}
 
-
-
-#' HTML stimulus, button response
-#'
-#' @param stimulus HTML stimulus
-#'
-#' @param choices Character vector with labels for the buttons
-#'
-#' @param button_html HTML templay specifying the buttons (defaults to jsPsych default)
-#' @param margin_vertical Vertical margin of the buttons (default "0px")
-#' @param margin_horizontal Horizontal margin of the buttons (default "8px")
-#'
-#' @param prompt The prompt to be displayed with the stimulus (can include HTML markup)
-#' @param stimulus_duration How long to display stimulus in milliseconds? (default is to remain until response)
-#' @param trial_duration How long to wait for response in milliseconds? (default is to wait indefinitely)
-#' @param response_ends_trial Does the trial end with response (default = TRUE) or continue until trial duration reached?
-#'
-#' @export
-html_button_response <- function(
-  stimulus,
-  choices,
-  button_html = NULL,
-  margin_vertical = "0px",
-  margin_horizontal = "8px",
-  prompt = NULL,
-  stimulus_duration = NULL,
-  trial_duration = NULL,
-  response_ends_trial = TRUE
-) {
-  drop_nulls(
-    trial(
-      type = "html-keyboard-response",
-      stimulus = stimulus,
-      choices = choices,
-      button_html = button_html,
-      margin_vertical = margin_vertical,
-      margin_horizontal = margin_horizontal,
       prompt = prompt,
       stimulus_duration = stimulus_duration,
       trial_duration = trial_duration,
@@ -99,6 +108,7 @@ html_button_response <- function(
 #' @export
 html_slider_response <- function(
   stimulus,
+
   labels = NULL,
   button_label = "Continue",
   min = 0,
@@ -107,6 +117,7 @@ html_slider_response <- function(
   step = 1,
   slider_width = NULL,
   require_movement = FALSE,
+
   prompt = NULL,
   stimulus_duration = NULL,
   trial_duration = NULL,
@@ -115,7 +126,9 @@ html_slider_response <- function(
   drop_nulls(
     trial(
       type = "html-keyboard-response",
+
       stimulus = stimulus,
+
       labels = labels,
       button_label = button_label,
       min = min,
@@ -124,6 +137,7 @@ html_slider_response <- function(
       step = step,
       slider_width = slider_width,
       require_movement = require_movement,
+
       prompt = prompt,
       stimulus_duration = stimulus_duration,
       trial_duration = trial_duration,
--- R/trial_image.R
+++ R/trial_image.R
@@ -22,28 +22,34 @@ image_button_response <- function(
   stimulus_height = NULL,
   stimulus_width = NULL,
   maintain_aspect_ratio = TRUE,
+
   choices = character(0),
   button_html = NULL,
+  margin_vertical = "0px",
+  margin_horizontal = "8px",
+
   prompt = NULL,
   stimulus_duration = NULL,
   trial_duration = NULL,
-  margin_vertical = "0px",
-  margin_horizontal = "8px",
   response_ends_trial = TRUE
 ) {
   drop_nulls(
     trial(
+      type = "image-button-response",
+
       stimulus = stimulus,
       stimulus_height = stimulus_height,
       stimulus_width = stimulus_width,
       maintain_aspect_ratio = maintain_aspect_ratio,
+
       choices = choices,
       button_html = button_html,
+      margin_vertical = margin_vertical,
+      margin_horizontal = margin_horizontal,
+
       prompt = prompt,
       stimulus_duration = stimulus_duration,
       trial_duration = trial_duration,
-      margin_vertical = margin_vertical,
-      margin_horizontal = margin_horizontal,
       response_ends_trial = response_ends_trial
     )
   )
@@ -72,30 +78,104 @@ image_keyboard_response <- function(
   stimulus_height = NULL,
   stimulus_width = NULL,
   maintain_aspect_ratio = TRUE,
+
   choices = any_key(),
+
   prompt = NULL,
   stimulus_duration = NULL,
   trial_duration = NULL,
-  margin_vertical = "0px",
-  margin_horizontal = "8px",
   response_ends_trial = TRUE
 ) {
   drop_nulls(
     trial(
+      type = "image-keyboard-response",
+
       stimulus = stimulus,
       stimulus_height = stimulus_height,
       stimulus_width = stimulus_width,
       maintain_aspect_ratio = maintain_aspect_ratio,
+
       choices = choices,
+
       prompt = prompt,
       stimulus_duration = stimulus_duration,
       trial_duration = trial_duration,
-      margin_vertical = margin_vertical,
-      margin_horizontal = margin_horizontal,
       response_ends_trial = response_ends_trial
     )
   )
 }
+
+
+
+
+#' Image stimulus, slider response
+#'
+#' @param stimulus Image file, specified using the resource() function
+#' @param stimulus_height Image height in pixels (defaults to natural height)
+#' @param stimulus_width Image width in pixels (defaults to natural width)
+#' @param maintain_aspect_ratio Maintain aspect ratio if only one of height/width specified? (default = TRUE)
+#'
+#' @param labels Character vector of labels, to be spaced equally along slider
+#' @param button_label Text displayed on button at the end (default = "Continue")
+#' @param min Minimum value of the slider (default = 0)
+#' @param max Maximum value of the slider (default = 100)
+#' @param start Starting value of the slider (default = 50)
+#' @param step Smallest increment of slider movement (default = 1)
+#' @param slider_width Horizontal width of slider (defaults to max width)
+#' @param require_movement Does user need to move the slider to continue? (default = FALSE)
+#'
+#' @param prompt The prompt to be displayed with the stimulus (can include HTML markup)
+#' @param stimulus_duration How long to display stimulus in milliseconds? (default is to remain until response)
+#' @param trial_duration How long to wait for response in milliseconds? (default is to wait indefinitely)
+#' @param response_ends_trial Does the trial end with response (default = TRUE) or continue until trial duration reached?
+#'
+#' @export
+image_slider_response <- function(
+  stimulus,
+  stimulus_height = NULL,
+  stimulus_width = NULL,
+  maintain_aspect_ratio = TRUE,
+
+  labels = NULL,
+  button_label = "Continue",
+  min = 0,
+  max = 100,
+  start = 50,
+  step = 1,
+  slider_width = NULL,
+  require_movement = FALSE,
+
+  prompt = NULL,
+  stimulus_duration = NULL,
+  trial_duration = NULL,
+  response_ends_trial = TRUE
+) {
+  drop_nulls(
+    trial(
+      type = "image-slider-response",
+
+      stimulus = stimulus,
+      stimulus_height = stimulus_height,
+      stimulus_width = stimulus_width,
+      maintain_aspect_ratio = maintain_aspect_ratio,
+
+      labels = labels,
+      button_label = button_label,
+      min = min,
+      max = max,
+      start = start,
+      step = step,
+      slider_width = slider_width,
+      require_movement = require_movement,
+
+      prompt = prompt,
+      stimulus_duration = stimulus_duration,
+      trial_duration = trial_duration,
+      response_ends_trial = response_ends_trial
+    )
+  )
+}
+
 
 
 
